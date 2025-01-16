9009e186c3fd36f7a3e9069feed7248f14bde6d2
benpar23 <par21015@byui.edu>
2025-01-15 20:46:22 -0500

    Started working on JS for this directory.html

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -15,3 +15,37 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const file = "/chamber/data/members.json";
+
+const cards = document.querySelector('.cards');
+
+async function getMemberData() {
+    const response = await fetch(file);
+    const data = await response.json();
+    // console.table(data.members);
+    displayMembers(data.members);
+}
+
+function displayMembers(members) {
+	members.forEach((member) => {
+        const card = document.createElement("section");
+        const name = document.createElement("h2");
+        const image = document.createElement("img");
+
+        name.innerHTML = `${member.name}`;
+
+        portrait.setAttribute("src", prophet.imageurl);
+        portrait.setAttribute("alt", `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute("loading", "lazy");
+        portrait.setAttribute("width", "200");
+        
+        card.appendChild(name);
+        card.appendChild(portrait);
+
+        cards.appendChild(card);
+	})
+}
+
+
+getMemberData();

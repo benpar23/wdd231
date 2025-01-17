421853139d15d009251ec20a5da923b9e0f3b60f
benpar23 <par21015@byui.edu>
2025-01-17 08:55:14 -0500

    small changes

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -54,3 +54,17 @@ function displayMembers(members) {
 
 
 getMemberData();
+
+const gridButton = document.querySelector("#grid");
+const listButton = document.querySelector("#list")
+const display = document.querySelector(".cards");
+
+gridButton.addEventListener("click", () => {
+	display.classList.add("cards");
+	display.classList.remove("list");
+});
+
+listButton.addEventListener("click", () => {
+    display.classList.add("list");
+    display.classList.remove("cards");
+})

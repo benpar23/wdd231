ef1552a4558907044b43fc5eccd0874e1549aab8
benpar23 <par21015@byui.edu>
2025-02-05 07:01:03 -0500

    Built out project folder

---
+++ chamber/scripts/discover.js
@@ -0,0 +1,17 @@
+const currentyear = document.querySelector("#currentyear");
+
+const lastModified = document.querySelector("#lastModified");
+
+const today = new Date();
+
+currentyear.innerHTML = today.getFullYear();
+
+lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});

f3a1c3d732dedb5b9c5bf1cf31df3080b2b54183
benpar23 <par21015@byui.edu>
2025-02-08 09:22:27 -0500

    Built out JS for discover page

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -15,3 +15,44 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+const url = "https://benpar23.github.io/wdd231/chamber/data/locations.json";
+
+const cards = document.querySelector('.cards');
+
+async function getLocationData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    displayLocations(data.locations);
+}
+
+function displayLocations(locations) {
+	locations.forEach((location) => {
+        const card = document.createElement("section");
+        const name = document.createElement("h2");
+        const image = document.createElement("img");
+		const address = document.createElement("p");
+
+        name.innerHTML = `${location.name}`;
+
+		address.innerHTML = `${location.address}`;
+        
+
+        image.setAttribute("src", location.image);
+        image.setAttribute("alt", `Picture of ${location.name}`);
+        image.setAttribute("loading", "lazy");
+        image.setAttribute("width", "200");
+        image.setAttribute("height", "200");
+
+        address.setAttribute("id", "address");
+        
+        card.appendChild(name);
+        card.appendChild(image);
+		card.appendChild(address);
+
+        cards.appendChild(card);
+	})
+}
+
+
+getLocationData();

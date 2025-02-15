cebc37b0e644bb2bb8faf88254d83cf855d92bde
benpar23 <par21015@byui.edu>
2025-02-15 17:35:48 -0500

    More changes

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -3,3 +3,26 @@ import { updateFooterDates, setupMenuToggle } from './global.js';
 updateFooterDates();
 
 setupMenuToggle();
+
+function showVisitorMessage() {
+
+	const dialog = document.querySelector("#popup");
+
+	const message = document.createElement("p");
+	message.innerHTML = "Welcome to Brownies Squared! A quick note that all orders will be confirmed via phone call after being placed.";
+	dialog.appendChild(message);
+
+
+	const button = document.querySelector(".close-button");
+
+	button.addEventListener("click", () => {
+
+		dialog.close();
+	})
+
+	dialog.showModal();
+}
+
+window.onload = function () {
+	setTimeout(showVisitorMessage, 2000);
+};
